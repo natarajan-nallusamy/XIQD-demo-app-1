cc791a7b379c5314893ab874715ebebcb63a8c98
natarajan-nallusamy <nnallusamy@extremenetworks.com>
2021-02-04 00:33:34 +0000

    Add sameple echo application

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const url = require('url');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	    var url_parts = url.parse(req.url, true);
+	    var query = url_parts.query;
+
+	    var message = (query && query.message) ? query.message : 'Hello World';
+	    var obj = {
+		            'message': message,
+		            'env': process.env
+		        };
+
+	    res.statusCode = 200;
+	    res.setHeader('Content-Type', 'application/json');
+	    res.end(JSON.stringify(obj));
+});
+
+server.listen(port, hostname, () => {
+	    console.log(`Server running at http://${hostname}:${port}/`);
+});
